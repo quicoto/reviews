39a74b9a2eba1ffc9a7d3cb3ca63310b5f938501
Ricard Torres <ricard.torres@netcentric.biz>
2020-05-18 15:20:23 +0200

    Change feed

---
+++ content/blog/boku-no-tonari-ni-ankoku-hakaishin-ga-imasu/1x3/index.md
@@ -0,0 +1,11 @@
+---
+title: Boku no Tonari ni Ankoku Hakaishin ga Imasu 1x3
+date: "2020-05-18T15:21:00"
+show: "Boku no Tonari ni Ankoku Hakaishin ga Imasu"
+season: 1
+episode: 3
+rating: 2
+ratingEmoji: "⭐️⭐️"
+---
+
+TEST : This should be sent via email
--- gatsby-config.js
+++ gatsby-config.js
@@ -72,14 +72,10 @@ module.exports = {
             serialize: ({ query: { site, allMarkdownRemark } }) => {
               return allMarkdownRemark.edges.map(edge => {
                 return Object.assign({}, edge.node.frontmatter, {
-                  description: edge.node.excerpt,
+                  description: `📺 Just watched: ${edge.node.frontmatter.title}\n\nRating: ${edge.node.frontmatter.ratingEmoji}\nReview: ${site.siteMetadata.siteUrl + edge.node.fields.slug}`,
                   date: edge.node.frontmatter.date,
                   url: site.siteMetadata.siteUrl + edge.node.fields.slug,
                   guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
-                  custom_elements: [
-                    { "content:encoded": edge.node.html },
-                    { "rating": edge.node.frontmatter.ratingEmoji }
-                  ],
                 })
               })
             },
