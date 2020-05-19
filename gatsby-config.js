2ed5435c7a9c46d1dd61de1c9aa3af2129648839
Ricard Torres <torres.rick@gmail.com>
2020-05-19 21:35:22 +0200

    Update packages

--- gatsby-config.js
+++ gatsby-config.js
@@ -142,8 +142,5 @@ module.exports = {
         pathToConfigModule: `src/utils/typography`,
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
   ],
 }
