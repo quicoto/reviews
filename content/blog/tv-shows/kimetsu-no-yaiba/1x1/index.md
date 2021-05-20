f40d346bef5dd4f47e7d0c76da66982f72f208b3
Ricard Torres <torres.rick@gmail.com>
2021-05-20 22:09:40 +0200

    Kimetsu no Yaiba 1x1

---
+++ content/blog/tv-shows/kimetsu-no-yaiba/1x1/index.md
@@ -0,0 +1,24 @@
+---
+type: series
+title: "Kimetsu no Yaiba 1x1"
+date: "2021-05-20T22:08:58"
+name: "Kimetsu no Yaiba"
+season: 1
+episode: 1
+rating: 4
+ratingEmoji: "⭐️⭐️⭐️⭐️"
+share: false
+---
+
+Of course, the super dependable big brother who takes care of everyone. Cliche much?
+
+Wait the dude has a special nose? Is he a and?
+I love how the old man invited him to spend the night and gave him dinner. Only in ancient Japan.
+
+Little did the big brother knew how his family would be slaughtered during the night. That was heavy.
+
+Wait, wasn't that enough?! Now the sister has become a demon. The dude is having a rough day.
+
+The cool-looking demon slayer has appeared and he quickly got super triggered when the main character begged for her sister's life.
+
+Overall nice animation, great backgrounds.
--- src/components/fileBuilder.js
+++ src/components/fileBuilder.js
@@ -2,22 +2,21 @@ import React from "react"
 import { StaticQuery, graphql } from "gatsby"
 const slugify = require("@sindresorhus/slugify")
 
-export default class FileBuilder extends React.Component{
-
+export default class FileBuilder extends React.Component {
   state = {
-    comment: '',
-    episode: '',
-    markdown: '',
+    comment: "",
+    episode: "",
+    markdown: "",
     rating: "3",
     season: "",
     show: "",
-    share: "0"
+    share: "0",
   }
 
   prepareMarkdown(state) {
-    const now = new Date();
-    let month = now.getMonth()+1;
-    let seconds = now.getSeconds();
+    const now = new Date()
+    let month = now.getMonth() + 1
+    let seconds = now.getSeconds()
     if (month < 10) month = `0${month}`
     if (seconds < 10) seconds = `0${seconds}`
     const date = `${now.getFullYear()}-${month}-${now.getDate()}T${now.getHours()}:${now.getMinutes()}:${seconds}`
@@ -25,9 +24,8 @@ export default class FileBuilder extends React.Component{
     let rating = ""
 
     for (let i = 0; i < +state.rating; i++) {
-      rating += '⭐️'
+      rating += "⭐️"
     }
-
 
     return `---
 type: series
@@ -38,128 +36,180 @@ season: ${+state.season}
 episode: ${+state.episode}
 rating: ${+state.rating}
 ratingEmoji: "${rating}"
-share: ${state.share === "1" ? 'true' : 'false'}
+share: ${state.share === "1" ? "true" : "false"}
 ---
 
-${state.comment ? state.comment : defaultComment }`
+${state.comment ? state.comment : defaultComment}`
   }
 
-  onChange = (event) => {
+  onChange = event => {
     this.setState({
-      [event.target.name]: event.target.value
+      [event.target.name]: event.target.value,
     })
   }
 
-  onClick = (event) => {
-    event.preventDefault();
-    const el = document.querySelector('[name="markdown"]');
-    el.select();
-    document.execCommand('copy');
+  onClick = event => {
+    event.preventDefault()
+    const el = document.querySelector('[name="markdown"]')
+    el.select()
+    document.execCommand("copy")
   }
-   render(){
-
+  render() {
     function Item(props) {
       const name = props.nodes[0].frontmatter.name
-      return (
-        <option value={name}>{name}</option>
-      )
+      return <option value={name}>{name}</option>
     }
 
-    return(
-     <div>
-      <form className="builder">
-            <div className="row">
-              <label htmlFor="show" className="form-label">TV Shows</label>
-              <select className="form-select" id="show" name="show" onChange={this.onChange} defaultValue={this.state.show}>
-                <option value="">Select TV Show</option>
-                <StaticQuery
-                  query={graphql`
-                    {
-                      allMarkdownRemark(
-                        sort: {
-                          order: [ASC, ASC]
-                          fields: [frontmatter___season, frontmatter___episode]
-                        }
-                        filter: { frontmatter: { type: { eq: "series" } } }
-                      ) {
-                        group(field: frontmatter___name) {
-                          nodes {
-                            fields {
-                              slug
-                            }
-                            frontmatter {
-                              date(formatString: "MMMM DD, YYYY")
-                              title
-                              name
-                              rating
-                              season
-                              episode
-                            }
-                            id
+    return (
+      <div>
+        <form className="builder">
+          <div className="row">
+            <label htmlFor="show" className="form-label">
+              TV Shows
+            </label>
+            <select
+              className="form-select"
+              id="show"
+              name="show"
+              onChange={this.onChange}
+              defaultValue={this.state.show}
+            >
+              <option value="">Select TV Show</option>
+              <StaticQuery
+                query={graphql`
+                  {
+                    allMarkdownRemark(
+                      sort: {
+                        order: [ASC, ASC]
+                        fields: [frontmatter___season, frontmatter___episode]
+                      }
+                      filter: { frontmatter: { type: { eq: "series" } } }
+                    ) {
+                      group(field: frontmatter___name) {
+                        nodes {
+                          fields {
+                            slug
                           }
-                          totalCount
+                          frontmatter {
+                            date(formatString: "MMMM DD, YYYY")
+                            title
+                            name
+                            rating
+                            season
+                            episode
+                          }
+                          id
                         }
+                        totalCount
                       }
                     }
-                  `}
-                  render={data => {
-                    let nodes = JSON.parse(JSON.stringify(data, null, 4)).allMarkdownRemark
-                      .group
+                  }
+                `}
+                render={data => {
+                  let nodes = JSON.parse(JSON.stringify(data, null, 4))
+                    .allMarkdownRemark.group
 
-                    return (
-                      nodes.map((item, index) => (
-                          <Item key={index.toString()} nodes={item.nodes} />
-                        ))
-                    )
-                  }}
-                ></StaticQuery>
-              </select>
-            </div>
-            <div className="row">
-              <label htmlFor="Season" className="form-label">Season</label>
-              <input id="Season" type="number" name="season"
-              onChange={this.onChange} />
-            </div>
-            <div className="row">
-              <label htmlFor="Episode" className="form-label">Episode</label>
-              <input id="Episode" type="number" name="episode"
-              onChange={this.onChange} />
-            </div>
-            <div className="row">
-              <label htmlFor="rating" className="form-label">Rating</label>
-              <select className="form-select" id="rating"
-              onChange={this.onChange} name="rating" defaultValue={this.state.rating}>
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-                <option value="5">5</option>
-              </select>
-            </div>
-            <div className="row">
-              <label htmlFor="Comment" className="form-label">Comment</label>
-              <textarea className="form-control" id="Comment" rows="3" name="comment" onChange={this.onChange}></textarea>
-            </div>
-            <div className="row">
-              <label htmlFor="markdown" className="form-label">Markdown</label>
-              <textarea readOnly className="form-control" id="markdown" rows="3" name="markdown" value={this.prepareMarkdown(this.state)}></textarea>
-            </div>
-            <div className="row">
-              <label htmlFor="share" className="form-label">Share</label>
-              <select className="form-select" id="share" name="share"
-              onChange={this.onChange} defaultValue={this.state.share}>
-                <option value="0">False</option>
-                <option value="1">True</option>
-              </select>
-            </div>
-            <div className="actions">
-              <button onClick={this.onClick}>Copy</button>
-              <a href={`https://github.com/quicoto/reviews/new/master/content/blog/tv-shows/?filename=tv-shows/${slugify(this.state.show)}/${this.state.season}x${this.state.episode}/index.md`}>Create file on Github</a>
-            </div>
-          </form>
-     </div>
+                  return nodes.map((item, index) => (
+                    <Item key={index.toString()} nodes={item.nodes} />
+                  ))
+                }}
+              ></StaticQuery>
+            </select>
+          </div>
+          <div className="row">
+            <label htmlFor="Season" className="form-label">
+              Season
+            </label>
+            <input
+              id="Season"
+              type="number"
+              name="season"
+              onChange={this.onChange}
+            />
+          </div>
+          <div className="row">
+            <label htmlFor="Episode" className="form-label">
+              Episode
+            </label>
+            <input
+              id="Episode"
+              type="number"
+              name="episode"
+              onChange={this.onChange}
+            />
+          </div>
+          <div className="row">
+            <label htmlFor="rating" className="form-label">
+              Rating
+            </label>
+            <select
+              className="form-select"
+              id="rating"
+              onChange={this.onChange}
+              name="rating"
+              defaultValue={this.state.rating}
+            >
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+            </select>
+          </div>
+          <div className="row">
+            <label htmlFor="Comment" className="form-label">
+              Comment
+            </label>
+            <textarea
+              className="form-control"
+              id="Comment"
+              rows="3"
+              name="comment"
+              onChange={this.onChange}
+            ></textarea>
+          </div>
+          <div className="row">
+            <label htmlFor="markdown" className="form-label">
+              Markdown
+            </label>
+            <textarea
+              readOnly
+              className="form-control"
+              id="markdown"
+              rows="3"
+              name="markdown"
+              value={this.prepareMarkdown(this.state)}
+            ></textarea>
+          </div>
+          <div className="row">
+            <label htmlFor="share" className="form-label">
+              Share
+            </label>
+            <select
+              className="form-select"
+              id="share"
+              name="share"
+              onChange={this.onChange}
+              defaultValue={this.state.share}
+            >
+              <option value="0">False</option>
+              <option value="1">True</option>
+            </select>
+          </div>
+          <div className="actions">
+            <button onClick={this.onClick}>Copy</button>
+            <a
+              href={`https://github.com/quicoto/reviews/new/master/content/blog/tv-shows/?filename=tv-shows/${slugify(
+                this.state.show
+              )}/${this.state.season}x${this.state.episode}/index.md`}
+            >
+              Create file on Github
+            </a>
+          </div>
+        </form>
+      </div>
     )
-   }
+  }
 }
 
 // const FileBuilder = props => (
