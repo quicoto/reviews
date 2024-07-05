135d3ad11709f522c166a8db97a6273deb81e422
Ricard Torres <ricard@ricard.dev>
2024-07-05 11:34:51 +0200

    The Promised Neverland volume 4

--- backend/batchCreationManga.js
+++ backend/batchCreationManga.js
@@ -9,8 +9,8 @@ import Paths from './paths.js';
   *******************
 */
 const showName = 'The Promised Neverland';
-const chaptersStart = 35; // Included
-const chaptersFinish = 43; // Included
+const chaptersStart = 26; // Included
+const chaptersFinish = 42; // Included
 const rating = 5;
 /*
   *******************
