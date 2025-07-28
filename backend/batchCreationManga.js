7eda23c0ed640a736610fb0fed8f571b92390d1a
Ricard Torres <ricard@ricard.dev>
2025-07-28 10:08:57 +0200

    Naruto 27

--- backend/batchCreationManga.js
+++ backend/batchCreationManga.js
@@ -9,8 +9,8 @@ import Paths from './paths.js';
   *******************
 */
 const showName = 'Naruto';
-const chaptersStart = 227; // Included
-const chaptersFinish = 235; // Included
+const chaptersStart = 236; // Included
+const chaptersFinish = 244; // Included
 const rating = 5;
 /*
   *******************
---
+++ content/blog/manga/naruto/243/index.md
@@ -0,0 +1,11 @@
+---
+type: manga
+title: Naruto
+date: "2025-07-28T00:00:00"
+name: "Naruto"
+chapter: 243
+rating: 5
+ratingEmoji: "⭐️⭐️⭐️⭐️⭐️"
+---
+
+_[No review was written for this episode]_
